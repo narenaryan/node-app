53d452b230888f83c2d70146da75b106c42fe2e4
Naren Yellavula <naren.yellavula@citrix.com>
2018-08-14 16:23:25 +0530

    Changed models to fit in all requirements

--- models/productModels.js
+++ models/productModels.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose');
 
 let Schema = mongoose.Schema;
+let ObjectId = Schema.ObjectId;
 
-// An article can have a straight parent which is a category
+// A category can have a category as parent and Many-to-Many relation with articles
+let categorySchema = new Schema({
+  _id: {type: String, required: true},
+  parent: {type: String, ref: 'Category', required: false},
+  children: [{type: String, ref: 'Category', required: false}],
+  articles: [{type: ObjectId, ref: 'Article'}]
+});
+
+// An article can have Many-to-Many relation with categories
 let articleSchema = new Schema({
   sku: {type: String, required: true},
   ean: {type: Number, required: true},
   name: {type: String, required: true},
   stockQuantity: {type: Number, required: true},
   price: {type: Number, required: true},
-  parent: {type: Schema.Types.ObjectId, ref: 'Category'}
-});
-
-// A category can have a category as parent and multiple categories/articles
-let categorySchema = new Schema({
-  _id: {type: String, required: true},
-  name: {type: String, required: true},
-  parent: {type: Schema.Types.ObjectId, ref: 'Category'},
-  children: [{type: Schema.Types.ObjectId, ref: 'Article', unique: true}, {type: String, ref: 'Category'}]
+  categories: [{type: ObjectId, ref: 'Category'}]
 });
 
 let Category = mongoose.model('Category', categorySchema);
