5fd3154c41bbc65b7bef6f0137b890e9770fe7bd
Naren Yellavula <naren.yellavula@citrix.com>
2018-08-14 13:47:27 +0530

    Added customer model to get JWT token to access API

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,3 +1,41 @@
+let models = require('../models/baseModels.js');
+let jwt = require('jsonwebtoken');
+let config = require('../config.js');
+
 exports.index = (req, res) => {
   res.send('NOT IMPLEMENTED: because it is Single Page App');
+};
+
+// First method to be called to receive JWT token
+exports.login = (req, res) => {
+  models.Customer.findOne({'email': req.body.email}, (err, customer) => {
+    if (err) {
+      res.status(500).send(err.message);
+    }
+    // Authenticate user
+    customer.comparePassword(req.body.password, (err, isMatch) => {
+      if (err) {
+        res.status(500).send(err.message);
+      }
+      if (isMatch) {
+        var token = jwt.sign({username: this.username},
+          config.secret,
+          { expiresIn: '24h' // expires in 24 hours
+          }
+        );
+
+        // return the JWT token for the future API calls
+        res.json({
+          success: true,
+          message: 'Authentication successful!',
+          token: token
+        });
+      } else {
+        res.json({
+          success: false,
+          message: 'Authentication failed!'
+        });
+      }
+    });
+  });
 };
---
+++ models/customerModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT = 10;
+
+let Schema = mongoose.Schema;
+
+// Models Customer document
+let customerSchema = new Schema({
+  username: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  firstName: {type: String, required: false},
+  lastName: {type: String, required: false}
+});
+
+// Convert string password to hash
+customerSchema.pre('save', (next) => {
+  let user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Note: Didn't use ES6 arrow syntax as it is giving lexical scope
+// Compare hashes of two passwords
+customerSchema.methods.comparePassword = function (userPassword, cb) {
+  bcrypt.compare(userPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+let Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = {
+  Customer: Customer
+};
